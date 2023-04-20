5685e950d08002c5ca92e5d452af1b17e6de957f
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-20 11:09:02 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,39 @@
 #include "myShell.h"
-int main()
-{
-  char *line;
-  char **args;
-  int status;
-  status = 0;
+#define MAX_COMMAND_LENGTH 256
 
-  do
-  {
-    printf(" ");
-    line = shell_read_line();
-    args = shell_split_line(line);
-    shell_exec(args);
+int main(void) {
+    char command[MAX_COMMAND_LENGTH]; // Almacena el comando ingresado por el usuario
+    int status; // Almacena el estado de ejecución del comando
 
-    free(line);
-    free(args);
+    while (1) {
+        printf("MiShell> "); // Mostrar el prompt
+        fgets(command, MAX_COMMAND_LENGTH, stdin); // Leer el comando ingresado por el usuario
+        command[strcspn(command, "\n")] = '\0'; // Eliminar el carácter de nueva línea al final del comando
 
-  } while (status);
+        if (strcmp(command, "exit") == 0) {
+            // Salir del intérprete cuando se ingresa el comando "exit"
+            break;
+        }
 
-  return 0;
+        pid_t pid = fork(); // Crear un nuevo proceso hijo
+
+        if (pid == 0) {
+            // Proceso hijo
+            char *args[] = {command, NULL}; // Crear un arreglo de argumentos para execve, el último elemento debe ser NULL
+            execve(command, args, environ); // Ejecutar el comando con execve, pasando el arreglo de argumentos y el entorno
+
+            // Si execve retorna, significa que ha ocurrido un error
+            perror("Error al ejecutar el comando");
+            exit(EXIT_FAILURE);
+        } else if (pid < 0) {
+            // Error al crear el proceso hijo
+            perror("Error al crear el proceso hijo");
+            exit(EXIT_FAILURE);
+        } else {
+            // Proceso padre
+            waitpid(pid, &status, 0); // Esperar a que el proceso hijo termine
+        }
+    }
+
+    return 0;
 }

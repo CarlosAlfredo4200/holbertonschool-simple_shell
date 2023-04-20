4f628e1676bc8a785d7f7f04ac68ff9d152c7865
jcamilop <6410@holbertonstudents.com>
2023-04-20 09:51:31 -0700

    builtins

--- builtins.c
+++ builtins.c
@@ -2,43 +2,48 @@
 #include "builtins.h"
 
 struct builtin builtins[] = {
-	    {"help", shell_help},
-	        {"exit", shell_exit},
-		    {"cd", shell_cd},
+{"help", shell_help},
+{"exit", shell_exit},
+{"cd", shell_cd},
 };
 
 void shell_exit(char **args __attribute__((unused)))
 {
-  exit(0);
+exit(0);
 }
 
 void shell_cd(char **args)
 {
-  if (args[1] == NULL)
-    {
-      fprintf(stderr, "shell: cd: missing argument not found\n");
-    }
-  else
-    {
-      if (chdir(args[1]) != 0)
-        {
-	  perror("shell: cd");
-        }
-    }
+if (args[1] == NULL)
+{
+fprintf(stderr, "shell: cd: missing argument not found\n");
+}
+else
+{
+if (chdir(args[1]) != 0)
+{
+perror("shell: cd");
+}
+}
 }
 
 void shell_help(char **args __attribute__((unused)))
 {
-    char *helptext =
-        "shell - the Kinda Aimless Shell. "
-        "The following commands are built in:\n"
-        "  cd       Change the working directory.\n"
-        "  exit     Exit the shell.\n"
-      "  help     Print this help text.\n";
-    printf("%s", helptext);
+char *helptext =
+
+"shell - the Kinda Aimless Shell.\n"
+"The following commands are built in:\n"
+"  cd       Change the working directory.\n"
+"  exit     Exit the shell.\n"
+"  help     Print this help text.\n";
+printf("%s", helptext);
+
 }
 
-int shell_num_builtins()
+int shell_num_builtins(void);
 {
-  return sizeof(builtins) / sizeof(struct builtin);
+
+(return sizeof(builtins) / sizeof(struct builtin));
+
 }
+

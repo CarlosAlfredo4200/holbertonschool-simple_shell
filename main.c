1638fa497a79d16c987e86ef711d627561d28d0e
jcamilop <6410@holbertonstudents.com>
2023-04-21 09:53:17 -0700

    main

---
+++ main.c
@@ -0,0 +1,21 @@
+nclude "myShell.h"
+
+
+
+int main(int ac, char **av, char **env)
+
+{
+
+	    if (ac == 1)
+
+		        {
+
+				       prompt(av, env);
+
+				           }
+
+	        
+
+	        return 0;
+
+}

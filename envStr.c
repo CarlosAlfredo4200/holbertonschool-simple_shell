1008016719d86c73929fb8c7842cdafe8b2684f2
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-20 21:01:22 -0500

    Update envStr.c

--- envStr.c
+++ envStr.c
@@ -4,19 +4,7 @@
  *
  * Return: 0
  */
-int envPrint(void)
-{
-	char *s = environ[0];
-	int i = 0;
 
-	while (s)
-	{
-		write(1, s, strlen(s));
-		write(1, "\n", 1);
-		s = environ[++i];
-	}
-	return (0);
-}
 
 /**
  * compStr - lexicographically compares not more than count chars

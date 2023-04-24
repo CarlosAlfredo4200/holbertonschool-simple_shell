430dbf162fc756fc7fc36880b66bd79a3a7e57b2
Holberton <holberton@Holbertons-iMac.local>
2023-04-24 13:13:05 -0500

    correction

--- shell.h
+++ shell.h
@@ -1,33 +1,81 @@
 #ifndef SHELL_H
 #define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <errno.h>
+#include <dirent.h>
+#include <signal.h>
 
 
-char *enter_line(void);
-char **tokenizer(char *string);
-char *find_path(char **env);
-int _putchar(char c);
-int tokenizer_path(char **arg, char **env);
-int run_com(char **arg, char **av, char **env, char *lineptr, int np, int c);
-char *length(char *str);
-void _getenv(char **env);
-void exit_shell(char **arg, char *lineptr, int _exit);
+/*constants*/
+#define EXTERNAL_COMMAND 1
+#define INTERNAL_COMMAND 2
+#define PATH_COMMAND 3
+#define INVALID_COMMAND -1
 
-char *_strcat(char *stringOne, char *stringTwo);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-char *_strchr(char *s, char c);
-int _strcmp(char *s1, char *s2);
-char *_strtok(char *str, char *deli);
-char *_strdup(char *string);
-unsigned int _strcspn(char *s, char *accept);
-size_t _strncmp(char *s1, char *s2, size_t n);
-#endif
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
+/**
+ *struct map - a struct that maps a command name to a function 
+ *
+ *@command_name: name of the command
+ *@func: the function that executes the command
+ */
+
+typedef struct map
+{
+	char *command_name;
+	void (*func)(char **command);
+} function_map;
+
+extern char **environ;
+extern char *line;
+extern char **commands;
+extern char *shell_name;
+extern int status;
+
+/*helpers*/
+void print(char *, int);
+char **tokenizer(char *, char *);
+void remove_newline(char *);
+int _strlen(char *);
+void _strcpy(char *, char *);
+
+/*helpers2*/
+int _strcmp(char *, char *);
+char *_strcat(char *, char *);
+int _strspn(char *, char *);
+int _strcspn(char *, char *);
+char *_strchr(char *, char);
+
+/*helpers3*/
+char *_strtok_r(char *, char *, char **);
+int _atoi(char *);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void ctrl_c_handler(int);
+void remove_comment(char *);
+
+/*utils*/
+int parse_command(char *);
+void execute_command(char **, int);
+char *check_path(char *);
+void (*get_func(char *))(char **);
+char *_getenv(char *);
+
+/*built_in*/
+void env(char **);
+void quit(char **);
+
+/*main*/
+extern void non_interactive(void);
+extern void initializer(char **current_command, int type_command);
+
+#endif /*SHELL_H*/
+
+

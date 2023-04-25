b997209059fcdcf30ab314879dbe3e9def1eaacb
camilo palacios <124897736+jcamilop@users.noreply.github.com>
2023-04-25 10:48:38 -0500

    Update builtin_functions.c

--- builtin_functions.c
+++ builtin_functions.c
@@ -60,3 +60,88 @@ void quit(char **tokenized_command)
 	else
 		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
 }
+
+/**
+ * tokenizer - tokenizes input and stores it into an array
+ *@input_string: input to be parsed
+ *@delim: delimiter to be used, needs to be one character string
+ *
+ *Return: array of tokens
+ */
+
+char **tokenizer(char *input_string, char *delim)
+{
+	int num_delim = 0;
+	char **av = NULL;
+	char *token = NULL;
+	char *save_ptr = NULL;
+
+	token = _strtok_r(input_string, delim, &save_ptr);
+
+	while (token != NULL)
+	{
+		av = _realloc(av, sizeof(*av) * num_delim, sizeof(*av) * (num_delim + 1));
+		av[num_delim] = token;
+		token = _strtok_r(NULL, delim, &save_ptr);
+		num_delim++;
+	}
+
+	av = _realloc(av, sizeof(*av) * num_delim, sizeof(*av) * (num_delim + 1));
+	av[num_delim] = NULL;
+
+	return (av);
+}
+
+/**
+ *remove_newline - removes new line from a string
+ *@str: string to be used
+ *
+ *
+ *Return: void
+ */
+
+void remove_newline(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			break;
+		i++;
+	}
+	str[i] = '\0';
+}
+
+/**
+ * ctrl_c_handler - handles the signal raised by CTRL-C
+ * @signum: signal number
+ *
+ * Return: void
+ */
+void ctrl_c_handler(int signum)
+{
+	if (signum == SIGINT)
+		print("\n($) ", STDIN_FILENO);
+}
+
+/**
+ * remove_comment - removes/ignores everything after a '#' char
+ * @input: input to be used
+ *
+ * Return: void
+ */
+void remove_comment(char *input)
+{
+	int i = 0;
+
+	if (input[i] == '#')
+		input[i] = '\0';
+	while (input[i] != '\0')
+	{
+		if (input[i] == '#' && input[i - 1] == ' ')
+			break;
+		i++;
+	}
+	input[i] = '\0';
+}
